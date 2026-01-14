8534a8793fadd672730f412f9214c1a162d4fd6d
mmand <matthewa054@gmail.com>
2026-01-14 00:28:28 -0500

    framework for adding performance benchmarks

---
+++ Ttt.Bot.Benchmarks/BotBench.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using TikTakNoMem;
+
+namespace Ttt.Bot.Benchmarks;
+
+[MemoryDiagnoser]
+public class BotBench
+{
+    private TikTakNoMem.Bot _bot;
+    private Board _board;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _bot = new TikTakNoMem.Bot();
+        _board = new Board(0,0);
+    }
+
+    [Benchmark]
+    public int GetBotBestMove_PrelimBenchmark()
+    {
+        return _bot.GetBestMove(_board, true);
+    }
+}
